367f794608dfeca990c35efa9c78716f8a34a3a1
LAPTOP-7CC4H83O\alexx <daalrive@espol.edu.ec>
2021-01-07 21:03:22 -0500

    agregacion de los controladores en el controlador

--- src/main/java/com/mycompany/proyecto_parcial2_2020/PrimaryController.java
+++ src/main/java/com/mycompany/proyecto_parcial2_2020/PrimaryController.java
@@ -2,11 +2,22 @@ package com.mycompany.proyecto_parcial2_2020;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+    @FXML
+    private Button btnOpciones;
+    @FXML
+    private Button btnCofiguraciones;
+    @FXML
+    private Button btnReporte;
+    @FXML
+    private Button btnuevoJuego;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+    //hacer metodos para hacer switch a las ventanas de configuracioes, reporte etc
 }
