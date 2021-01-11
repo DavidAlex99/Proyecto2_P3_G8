bfb9cdc56745172f52218e825c4ae194a4fbb4a9
Kaylav4 <keyybarc@espol.edu.ec>
2021-01-10 22:35:24 -0500

    Clase creadas: alineacion, configuracion, carta, cartajuego y mazo

---
+++ src/main/java/modelo/Configuracion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author cholo
+ */
+public class Configuracion {
+    private boolean visible;
+    private int numOponentes;
+    
+    public Configuracion( boolean visible, int numOponentes){
+        this.visible = visible;
+        this.numOponentes = numOponentes;    
+    }
+    public boolean getVisible(){
+        return visible;
+    }
+    public int getOponentes(){
+        return numOponentes;
+    }
+    
+ //metodo que no entiendo 
+}
