2b825851a1727d804704fc8d257d2c717007fa16
LAPTOP-7CC4H83O\alexx <daalrive@espol.edu.ec>
2021-01-14 22:13:02 -0500

    metodos para poner las cartas en el tablero y las cartas intercambiantes

--- src/main/java/com/mycompany/proyecto_parcial2_2020/VentanaJuegoController.java
+++ src/main/java/com/mycompany/proyecto_parcial2_2020/VentanaJuegoController.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,7 +32,10 @@ import modelo.Carta;
  */
 public class VentanaJuegoController implements Initializable {
 
+    public static ArrayList<Integer> indices;
     public static ArrayList<Carta> cartas = new ArrayList<>();
+    public static ArrayList<ImageView> panelesimages = new ArrayList<>();
+    
     @FXML
     private VBox vboxCartas;
     @FXML
@@ -46,6 +50,38 @@ public class VentanaJuegoController implements Initializable {
     private ImageView imgCartaMazo;
     @FXML
     private Button btnLoteria;
+        @FXML
+    private ImageView imgCarta1;
+    @FXML
+    private ImageView imgCarta2;
+    @FXML
+    private ImageView imgCarta3;
+    @FXML
+    private ImageView imgCarta4;
+    @FXML
+    private ImageView imgCarta5;
+    @FXML
+    private ImageView imgCarta6;
+    @FXML
+    private ImageView imgCarta7;
+    @FXML
+    private ImageView imgCarta8;
+    @FXML
+    private ImageView imgCarta9;
+    @FXML
+    private ImageView imgCarta10;
+    @FXML
+    private ImageView imgCarta11;
+    @FXML
+    private ImageView imgCarta12;
+    @FXML
+    private ImageView imgCarta13;
+    @FXML
+    private ImageView imgCarta14;
+    @FXML
+    private ImageView imgCarta15;
+    @FXML
+    private ImageView imgCarta16;
     /**
      * Initializes the controller class.
      */
@@ -108,6 +144,50 @@ public class VentanaJuegoController implements Initializable {
     }catch (IOException ex) {
             System.out.println(ex + "no se encuentra");
 }
+}
+    
+        //3. METODO PARA PNER LAS CARTAS EN EL TABLERO
+    public void ponerCartasTablero(){
+        //3.1 llenado de la sta cartas con os indices de las cartas
+        indices = new ArrayList<Integer>();
+        int ind = 0;
+        while(ind<54){
+            indices.add(ind + 1);
+            ind ++;
+        }
+        ///////////////////////////////////////////////////////////
+        Random x = new Random();
+        ///////////////////////////////////////////////////////////
+        // ITERADOR PARA LA CANTIDAD DE CARTAS QUE E NECESIA PARA COMPLETAR EL TABLERO
+        int i = 0;
+        //ITERADOR PARA LAS POSICIONES DE LAS CARTAS LAS CARATAS QUE TENGO DISPONIBLE EL MAZO COMPLETOPARA EL TABLERO
+        int x1 = 54;
+        while(i<17){
+            //3.2 ESO ME DEVUELVE UN NUMERO RADOM DENTRO DEL LIMITE, NUMERO RANDOM QUE REPRESETARIA UN INDICE
+            int n = x.nextInt(x1 - 1);
+            //3.3 DEVUELVE LA CARTA POR EL INDICE, EN ESTE CASO EL NUMERO DE LA CARTA
+            int card = indices.get(n);
+            indices.remove(n);
+
+            for(Carta c1: cartas){
+                if(c1.getNumero()==n){
+                    InputStream input = null;
+                    try{
+                        //3.6 PARA ESTO LE HE PUESTO NOMBRE A CADA IMAGEVIEW EN LA MATRIZ DE BAAJAS
+                        String fileName = App.pathimagenes + c1.getRutaImagen();
+                        input = App.class.getResource(fileName).openStream();
+                        Image image = new Image(input, 100, 100, false , false);
+                        panelesimages.get(i).setImage(image);
+                    }catch(IOException ex){
+                        System.out.println("No se puede cargar la imagen");   
+        }
+    }
+}
+            //3.4 AUMENTO EL ITERADOR EN 1
+            i++;
+            //3.5RESTA DEL NUMERO DE CAARTAS QUE SE ENCUENTRA EN EL MAZO
+            x1 = x1 -1;
+            }
 }
 
    
